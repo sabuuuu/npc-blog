6076095bc5a3b925c9d42705bb575278ea5ebba1
Sabrina Yafa <yafasabrina2323@gmail.com>
2024-07-09 22:05:29 +0100

    Padding issues on small screens

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -20,19 +20,19 @@ const PostCard = ({ post }) => {
             <p className="text-gray-600 ml-4  text-lg">{author.name}</p>
           </div>
         )}
-        <div className="text-gray-600 ml-4 text-lg flex">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-[#351447]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <div className="text-gray-600 ml-2 lg:ml-4 md:ml-4 text-lg flex">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 lg:h-6 lg:w-6  md:h-6 md:w-6 inline mr-2 text-[#351447]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
           </svg>
-          <span className="">{moment(post.createdAt).format('DD MMMM, YYYY')}</span>
+          <span className="text-xs lg:text-lg md:text-lg">{moment(post.createdAt).format('DD MMMM, YYYY')}</span>
         </div>
       </div>
       <h1 className="text-xl font-extrabold mt-4 text-center text-[#f4ecfb] transition duration-300 hover:text-[#351447] mb-2">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <p className="text-center text-lg font-medium text-[#351447] mb-6">{post.excerpt}</p>
-      <div className="text-center">
-        <Link href={`/post/${post.slug}`} className="transition duration-300 text-[#f4ecfb] font-medium hover:bg-[#502b64] bg-[#351447] px-16 py-2 rounded-lg shadow-xl">Continue reading</Link>
+      <div className="text-center w-full">
+        <Link href={`/post/${post.slug}`} className="transition duration-300 text-[#f4ecfb] font-medium hover:bg-[#502b64] bg-[#351447] px-6 lg:px-16 md:px-16 py-2 rounded-lg shadow-xl w-full">Continue reading</Link>
       </div>
     </div>
   );
